2f22302423687a4046cf9d413b807c10aaad3dfe
Gowthami Bommasani <gowthamib@infinite.com>
2024-11-03 16:39:24 -0800

    Assignment-2

---
+++ assignment 2/assignment 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_copy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            int[] b = new int[5];
+            int i = 0;
+            while (i < b.Length)
+            {
+                b[i] = a[i];
+                i++;
+
+            }
+
+            foreach (int value in b)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
