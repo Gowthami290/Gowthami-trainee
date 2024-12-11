18aceb3900ce9ff907af227f0b0233d58a9c15be
Gowthami Bommasani <gowthamib@infinite.com>
2024-12-11 12:58:20 +0530

    Assessment

---
+++ C Sharp-assessment/Assessment-5/Employee/Employee/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=YourServer;Initial Catalog=YourDatabase;Integrated Security=True";
+        string productName = "New Product";
+        decimal price = 100.00m;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand command = new SqlCommand("sp_InsertProduct", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ProductName", productName);
+                command.Parameters.AddWithValue("@Price", price);
+                command.Parameters.Add("@ProductId", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                command.ExecuteNonQuery();
+
+                int productId = (int)command.Parameters["@ProductId"].Value;
+                decimal discountedPrice = GetDiscountedPrice(productId, connection);
+
+                Console.WriteLine("Product ID: " + productId);
+                Console.WriteLine("Discounted Price: " + discountedPrice);
+            }
+        }
+    }
+
+    static decimal GetDiscountedPrice(int productId, SqlConnection connection)
+    {
+        using (SqlCommand command = new SqlCommand("SELECT DiscountedPrice FROM ProductsDetails WHERE ProductId = @ProductId", connection))
+        {
+            command.Parameters.AddWithValue("@ProductId", productId);
+
+            return (decimal)command.ExecuteScalar();
+        }
+    }
+}
